74d9049ae45e7f702098fae9f730da5cec4cde2c
YanaIvanova21 <yana.ivanova.199621@gmail.com>
2023-03-22 22:53:57 +0200

    Bug fixed.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const boxes = document.querySelectorAll(".box");
-
 const triggerLine = (window.innerHeight / 5) * 4;
-console.log(triggerLine);
-
 window.addEventListener("scroll", onScrollEventHandler);
+
+onScrollEventHandler();
 
 function onScrollEventHandler() {
   boxes.forEach((box) => showBox(box));
 }
 
 function showBox(box) {
-  if (document.documentElement.scrollTop > triggerLine) {
+  const boxTop = box.getBoundingClientRect().top;
+  if (boxTop < triggerLine) {
     box.classList.add("show");
-  } else box.classList.remove("show");
+  } else {
+    box.classList.remove("show");
+  }
 }
